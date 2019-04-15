31d872e0de85d4f308f97334017c2d622232f583
Abdurrahman Akkas <akkas@mit.edu>
2019-04-15 14:12:43 -0400

    install bash_history

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 set -x
 ln -snf $PWD/.vimrc ~/.vimrc
 ln -snf $PWD/.screenrc ~/.screenrc
-# We want to keep local bash options if any:
+# We want to keep local bash options if any, so import instead of linking
 source_cmd="source $PWD/.bash_profile"
-if [ "$(cat ~/.bash_profile | tail -1)" != "$source_cmd" ];
+if ! grep -Fxq "$source_cmd" ~/.bash_profile
 then
     echo "# .dotfiles source:" >> ~/.bash_profile
     echo $source_cmd >> ~/.bash_profile
