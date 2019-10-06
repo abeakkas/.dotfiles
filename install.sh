4dac570afcc94167b258dc40b94682b6c8a05c28
Abdurrahman Akkas <akkas@mit.edu>
2019-10-06 14:12:31 -0400

    Make files visible

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 set -x
 mkdir -p ~/.vim/tmp
-ln -snf $PWD/.vimrc ~/.vimrc
-ln -snf $PWD/.screenrc ~/.screenrc
+ln -snf $PWD/vimrc ~/.vimrc
+ln -snf $PWD/screenrc ~/.screenrc
 # We want to keep local bash options if any, so import instead of linking
-source_cmd="source $PWD/.bash_profile"
+source_cmd="source $PWD/bash_profile"
 if ! grep -Fxq "$source_cmd" ~/.bash_profile
 then
     echo "# .dotfiles source:" >> ~/.bash_profile
